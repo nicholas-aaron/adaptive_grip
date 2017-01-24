8110b381debd41e0d2b130fb53c500f5fbed30da
SamRMason <sam.r.mason@gmail.com>
2017-01-24 13:02:14 -0500

    Updating code

--- arduino/Individual_motor_test/Individual_motor_test.ino
+++ arduino/Individual_motor_test/Individual_motor_test.ino
@@ -1,6 +1,12 @@
 //Define constants for Enable, Step, Direction, MS1 and MS2 pins
 
-#define EN  2   //Blue
+//Available digital pins are: 0,1,2,13
+#define EN_1 7
+#define EN_2 8
+#define EN_3 9
+#define EN_4 10
+#define EN_5 11
+#define EN_6 12
 #define MS1 3   //Green
 #define MS2 4   //Yellow
 #define stp 5   //Orange
@@ -19,7 +25,12 @@ int count4;
 
 void setup() {
   // put your setup code here, to run once:
-  pinMode(EN,OUTPUT);
+  pinMode(EN_1,OUTPUT);
+  pinMode(EN_2,OUTPUT);
+  pinMode(EN_3,OUTPUT);
+  pinMode(EN_4,OUTPUT);
+  pinMode(EN_5,OUTPUT);
+  pinMode(EN_6,OUTPUT);
   pinMode(stp,OUTPUT);
   pinMode(dir,OUTPUT);
   pinMode(MS1,OUTPUT);
@@ -45,20 +56,58 @@ void setup() {
 
 //Main loop
 void loop() {
-  while(Serial.available()){
+  
+    while(Serial.available()){
       user_input = Serial.read(); //Read user input and trigger appropriate function
-      digitalWrite(EN, LOW); //Pull enable pin low to allow motor control
+      //digitalWrite(EN, LOW); //Pull enable pin low to allow motor control
+      for (int motorNumber = EN_1; motorNumber <=EN_6; motorNumber++)
+    {
+      String motorNum = String(motorNumber);
+      Serial.println("Moving motor number " + motorNum);
+      controlMotor(motorNumber);
       controlStep();
-      controlMotor();
-      
       resetEDPins();
       holdPosition();
+    }
   }
 }
 
 void controlMotor(int motorID)
 {
-  // This function ensures that only one motor runs at a time
+  switch(motorID)
+  {
+    //Serial.println("Moving Motor Number" + motorID);
+    case EN_1: //(7)NEMA23
+    {
+      digitalWrite(EN_1,LOW);
+      break;
+    }
+    case EN_2: //(8)NEMA23
+    {
+      digitalWrite(EN_2,LOW);
+      break;
+    }
+    case EN_3: //(9)NEMA23
+    {
+      digitalWrite(EN_3,LOW);
+      break;
+    }
+    case EN_4: //(10)NEMA17
+    {
+      digitalWrite(EN_4,LOW);
+      break;
+    }
+    case EN_5: //(11)NEMA17
+    {
+      digitalWrite(EN_5,LOW);
+      break;
+    }
+    case EN_6: //(12)NEMA17
+    {
+      digitalWrite(EN_6,LOW);
+      break;
+    }
+  }
   
 }
 
@@ -96,7 +145,7 @@ void controlStep()
 void holdPosition()
 {
   // Keep enable low to hold position
-  digitalWrite(EN,LOW);  
+  //digitalWrite(EN,LOW);  
 }
 
 void FullStep()
@@ -114,7 +163,7 @@ void FullStep()
     //Serial.println(count);
   }
   count1+=999;
-  Serial.println("Enter new option");
+  Serial.println("\nEnter new option");
   Serial.println();
 }
 
@@ -232,6 +281,11 @@ void resetEDPins()
   digitalWrite(dir, LOW);
   digitalWrite(MS1, LOW);
   digitalWrite(MS2, LOW);
-  digitalWrite(EN, HIGH);
+  digitalWrite(EN_1, HIGH);
+  digitalWrite(EN_2, HIGH);
+  digitalWrite(EN_3, HIGH);
+  digitalWrite(EN_4, HIGH);
+  digitalWrite(EN_5, HIGH);
+  digitalWrite(EN_6, HIGH);
 }
 
