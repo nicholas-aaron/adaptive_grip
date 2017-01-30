d364ce5ce5a735985e930c01ff7a423884c588ee
SamRMason <sam.r.mason@gmail.com>
2017-01-30 14:16:21 -0500

    Working code for move motor and hold position

--- arduino/Separate_Direction_Control/Motor_Control.ino
+++ arduino/Separate_Direction_Control/Motor_Control.ino
@@ -1,106 +1,9 @@
-<<<<<<< HEAD
-=======
-/**
- * function: controlMotor
- * Enables one of the 6 gripper motors to turn by setting its enable pin to "LOW"
- * 
- * motorID: integer ID of the motor
- */
-
-//void controlMotor(int motorID)
-//{
-//  switch(motorID)
-//  {
-//    //Serial.println("Moving Motor Number" + motorID);
-//    case EN_1: //(7)NEMA23
-//    {
-//      digitalWrite(EN_1,LOW);
-//      break;
-//    }
-//    case EN_2: //(8)NEMA23
-//    {
-//      digitalWrite(EN_2,LOW);
-//      break;
-//    }
-//    case EN_3: //(9)NEMA23
-//    {
-//      digitalWrite(EN_3,LOW);
-//      break;
-//    }
-//    case EN_4: //(10)NEMA17
-//    {
-//      digitalWrite(EN_4,LOW);
-//      break;
-//    }
-//    case EN_5: //(11)NEMA17
-//    {
-//      digitalWrite(EN_5,LOW);
-//      break;
-//    }
-//    case EN_6: //(12)NEMA17
-//    {
-//      digitalWrite(EN_6,LOW);
-//      break;
-//    }
-//  }
-//  
-//}
->>>>>>> f82b96b97e11b0c3bf8c3e1155593b51e1d337b6
-
-/**
- * Function: driveMotor
- * 
- * Rotates the motor(s) by a specified number of degrees at full, half, quarter or eight step speed in
- * either clockwise or counter-clockwise direction
- * 
- * degrees: amount by which the motor is rotated
- * stepStyle: absolute change in angular displacement motor takes with each 'step'
- * highOrLow: the direction that the motor will rotate; CW vs CCW
- */
-
-<<<<<<< HEAD
-double selectStepSize(int stpSize){
-  double degreesPerStep = 0;
-  switch(stpSize){
-  case 0: { // Full Step
-    //Serial.println("Moving at full step mode.");
-    digitalWrite(MS1, LOW); 
-    digitalWrite(MS2, LOW);
-    degreesPerStep = (double)(1.8/1);
-    return degreesPerStep;
-    break;
-  }
-  case 1: { // Half Step
-    //Serial.println("Moving at half step mode.");
-    digitalWrite(MS1, HIGH); 
-    digitalWrite(MS2, LOW);
-    degreesPerStep = (double)(1.8/2);
-    return degreesPerStep;
-    break;
-  }
-  case 2: { // Quarter Step
-    //Serial.println("Moving at quarter step mode.");
-    digitalWrite(MS1, LOW); 
-    digitalWrite(MS2, HIGH);
-    degreesPerStep = (double)(1.8/4);
-    return degreesPerStep;
-    break;
-  }
-  default: { // Eighth Step
-    //Serial.println("Moving at eigth step mode.");
-    digitalWrite(MS1, HIGH); 
-    digitalWrite(MS2, HIGH);
-    degreesPerStep = (double)(1.8/8);
-    return degreesPerStep;
-    break;
-  }
-  }
-}
-
-void driveMotor(int mtrID, int degRot, boolean rotDir, double degPerStp, boolean dirSwitch )
+void driveMotor(int mtrID, int degRot, boolean rotDir )
 {
+  double degPerStp = (double)(1.8/8); // This is used for 1/8th stepping mode
   int numSteps = (int)(degRot/degPerStp); 
-  for(x= 1; x<numSteps; x++)  //Loop the forward stepping enough times for motion to be visible
+  boolean dirSwitch = false;
+  for(int x=0; x<numSteps; x++)  //Loop the forward stepping enough times for motion to be visible
   {
     if (dirSwitch){
       digitalWrite(DIR_1,HIGH);
@@ -128,114 +31,22 @@ void driveMotor(int mtrID, int degRot, boolean rotDir, double degPerStp, boolean
     }
     
     delay(2);
-    digitalWrite(stp, HIGH);
+    digitalWrite(STP, HIGH);
     delay(2);
-    digitalWrite(stp,LOW);
+    digitalWrite(STP,LOW);
   }
 }
-=======
-//void driveMotor(int degrees, int stepStyle, uint8_t highOrLow )
-//{
-//  int rotate = 0;
-//  digitalWrite(dir,highOrLow);
-//switch(stepStyle){
-//  case 0: { // Full Step
-//    Serial.println("Moving at full step mode.");
-//    digitalWrite(MS1, LOW); 
-//    digitalWrite(MS2, LOW);
-//    rotate = (int)(degrees/1.8);
-//    break;
-//  }
-//  case 1: { // Half Step
-//    Serial.println("Moving at half step mode.");
-//    digitalWrite(MS1, HIGH); 
-//    digitalWrite(MS2, LOW);
-//    rotate = 2*(int)(degrees/1.8);
-//    break;
-//  }
-//  case 2: { // Quarter Step
-//    Serial.println("Moving at quarter step mode.");
-//    digitalWrite(MS1, LOW); 
-//    digitalWrite(MS2, HIGH);
-//    rotate = 4*(int)(degrees/1.8);
-//    break;
-//  }
-//  default: { // Eighth Step
-//    Serial.println("Moving at eigth step mode.");
-//    digitalWrite(MS1, HIGH); 
-//    digitalWrite(MS2, HIGH);
-//    rotate = 8*(int)(degrees/1.8);
-//    break;
-//  }
-//}
-// 
-//  for(x= 1; x<rotate; x++)  //Loop the forward stepping enough times for motion to be visible
-//  {
-//    digitalWrite(stp,HIGH); //Trigger one step forward
-//    delay(1);
-//    digitalWrite(stp,LOW); //Pull step pin low so it can be triggered again
-//    delay(1);
-//    count2++;
-//  }
-//}
->>>>>>> f82b96b97e11b0c3bf8c3e1155593b51e1d337b6
 
-
-/**
- * Function: Reset pins
- * 
- * Disables all 6 motors from moving.  Changes motor step size to full step.
- */
 void setPins()
 {
-<<<<<<< HEAD
-  digitalWrite(stp, LOW);
+  digitalWrite(STP, LOW);
   digitalWrite(DIR_1, LOW); // All moving in the same direction
   digitalWrite(DIR_2, LOW);
   digitalWrite(DIR_3, LOW);
   digitalWrite(DIR_4, LOW);
   digitalWrite(DIR_5, LOW);
   digitalWrite(DIR_6, LOW);
-  digitalWrite(MS1, LOW);
-  digitalWrite(MS2, LOW);
-  digitalWrite(EN, LOW);
-}
 
-void holdPosition()
-{ // Holds a position by vibrating back and forth very, very quickly
-  boolean dirSwitch = true;
-  for (int i = 0; i < 1000; i++){
-    if (dirSwitch){
-      digitalWrite(DIR_1,HIGH);
-      digitalWrite(DIR_2,HIGH);
-      digitalWrite(DIR_3,HIGH);
-      digitalWrite(DIR_4,HIGH);
-      digitalWrite(DIR_5,HIGH);
-      digitalWrite(DIR_6,HIGH);
-      dirSwitch = !dirSwitch;
-    }
-    else{
-      digitalWrite(DIR_1,LOW);
-      digitalWrite(DIR_2,LOW);
-      digitalWrite(DIR_3,LOW);
-      digitalWrite(DIR_4,LOW);
-      digitalWrite(DIR_5,LOW);
-      digitalWrite(DIR_6,LOW);
-      dirSwitch = !dirSwitch;
-    }
-
-    delay(2);
-    digitalWrite(stp,HIGH);
-    delay(2);
-    digitalWrite(stp, LOW);
-=======
-  digitalWrite(STP, LOW);
-  digitalWrite(DIR_1, HIGH);
-  digitalWrite(DIR_2, HIGH);
-  digitalWrite(DIR_3, HIGH);
-  digitalWrite(DIR_4, HIGH);
-  digitalWrite(DIR_5, HIGH);
-  digitalWrite(DIR_6, HIGH);
 }
 
 void holdPosition(int mSecs)
@@ -261,6 +72,5 @@ void holdPosition(int mSecs)
     delay(1);
     digitalWrite(STP,LOW);
     delay(4);
->>>>>>> f82b96b97e11b0c3bf8c3e1155593b51e1d337b6
   }
 }
--- arduino/Separate_Direction_Control/Separate_Direction_Control.ino
+++ arduino/Separate_Direction_Control/Separate_Direction_Control.ino
@@ -8,14 +8,6 @@
 
 //Declare variables for functions
 char userInput;
-int desiredDegrees;
-int x;
-int y;
-int state;
-int count1;
-int count2;
-int count3;
-int count4;
 
 void setup() {
   pinMode(STP,OUTPUT);
@@ -29,99 +21,22 @@ void setup() {
   setPins();
   Serial.begin(9600); //Open Serial connection for debugging
   //Print function list for user selection
-  Serial.println("Ready to Test?");
+  Serial.println("Test Begins");
   Serial.println();
-  
-  count1 = 0;
-  count2 = 0;
-  count3 = 0;
-  count4 = 0;
 }
 
 //Main loop
 void loop() {
-<<<<<<< HEAD
-  int degrees = 90;
-  double degreesPerStep = 0;
-  boolean rotationDirection = true;
-  boolean dirSwitch = true;
-
-  selectStepSize(1); //choose half stepping
+  int degrees = 180;
+  boolean rotationDirection = false;
+  
   for(int motorNum = DIR_1; motorNum <= DIR_6; motorNum++){
-    driveMotor(motorNum, degrees,rotationDirection, degreesPerStep, dirSwitch); 
+    driveMotor(motorNum, degrees,rotationDirection); 
   }
-  holdPosition();
+  holdPosition(5000);
 }
 
 
-=======
-      
-      userInput = Serial.read(); //Read user input and trigger appropriate function 
-      resetEDPins();
-      for(x= 0; x<1000; x++)  //Loop the forward stepping enough times for motion to be visible
-      {
-        digitalWrite(STP,HIGH); //Trigger one step forward
-        delay(1);
-        digitalWrite(STP,LOW); //Pull step pin low so it can be triggered again
-        delay(1);
-      }
-
-      holdPosition(10000);  
-}
-
-
-
-
-
-
-//void OriginalPosition()
-//{
-//  Serial.println("Move back to origianl position");
-//  digitalWrite(dir, HIGH); //Pull direction pin high to move "forward"
-//  
-//  digitalWrite(MS1, LOW); 
-//  digitalWrite(MS2, LOW);
-//  for(x= 1; x< count1; x++)  //Loop the forward stepping enough times for motion to be visible
-//  {
-//    digitalWrite(stp,HIGH); //Trigger one step forward
-//    delay(1);
-//    digitalWrite(stp,LOW); //Pull step pin low so it can be triggered again
-//    delay(1);
-//  }
-//
-//  digitalWrite(MS1, HIGH); 
-//  digitalWrite(MS2, LOW);
-//  for(x= 1; x< count2; x++)  //Loop the forward stepping enough times for motion to be visible
-//  {
-//    digitalWrite(stp,HIGH); //Trigger one step forward
-//    delay(1);
-//    digitalWrite(stp,LOW); //Pull step pin low so it can be triggered again
-//    delay(1);
-//  }
-//
-//  digitalWrite(MS1, LOW); 
-//  digitalWrite(MS2, HIGH);
-//  for(x= 1; x< count3; x++)  //Loop the forward stepping enough times for motion to be visible
-//  {
-//    digitalWrite(stp,HIGH); //Trigger one step forward
-//    delay(1);
-//    digitalWrite(stp,LOW); //Pull step pin low so it can be triggered again
-//    delay(1);
-//  }
-//
-//  digitalWrite(MS1, HIGH); 
-//  digitalWrite(MS2, HIGH);
-//  for(x= 1; x< count4; x++)  //Loop the forward stepping enough times for motion to be visible
-//  {
-//    digitalWrite(stp,HIGH); //Trigger one step forward
-//    delay(1);
-//    digitalWrite(stp,LOW); //Pull step pin low so it can be triggered again
-//    delay(1);
-//  }
-//  Serial.println("Enter new option");
-//  Serial.println();
-//}
->>>>>>> f82b96b97e11b0c3bf8c3e1155593b51e1d337b6
 
 
 
