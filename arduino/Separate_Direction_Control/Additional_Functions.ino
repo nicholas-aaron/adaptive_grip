be93b1f82e3175cb8c893c42c7d1aabfea46dee6
SamRMason <sam.r.mason@gmail.com>
2017-02-13 17:06:59 -0500

    Effective Grabbing Code

--- arduino/Separate_Direction_Control/Additional_Functions.ino
+++ arduino/Separate_Direction_Control/Additional_Functions.ino
@@ -33,7 +33,7 @@ void printPositions(int *positions) {
   Serial.println("");
 }
 
-void userMotorChoice(char userInput, int (*motors), boolean directions[], int (*force))
+void userMotorChoice(char userInput, int (*motors), boolean directions[], int (*force), int (*innerLimits), int (*outerLimits), int (*forceThreshold))
 {
   switch (userInput) {
     case '1': {
@@ -85,23 +85,17 @@ void userMotorChoice(char userInput, int (*motors), boolean directions[], int (*
         directions[5] = false;
         break;
     } case 'e': {
-        openAllJoints(motors,directions,positions,limits);
-        curlGrab(motors, directions, positions, curlLimits, force);
+        openAllJoints(motors,directions,positions,outerLimits);
+        curlGrab(motors, directions, positions, innerLimits, force, forceThreshold);
         break;
     } case 'f':{
-        closeAllJoints(motors,directions,positions);
+        int homeLimits[6] = {0,0,0,0,0,0};
+        closeAllJoints(motors,directions,positions, homeLimits);
         break;
     } case 'h': {
-        openAllJoints(motors,directions,positions,limits);
+        openAllJoints(motors,directions,positions,outerLimits);
         break;
-    } case 'i': {
-        openAllJoints(motors,directions,positions,limits);
-        curlGrab(motors,directions,positions,clampLimits,force);
-        break;
-    } case 'j': {
-        grab(motors, directions,positions,innerLimits, outerLimits);
-        break;
-    } 
+    }
 
     case 'z': {
         firstRun = true;
@@ -109,10 +103,12 @@ void userMotorChoice(char userInput, int (*motors), boolean directions[], int (*
         break;
     } case 'k': {
         checkPressure(force);
-        Serial.println(force[0]);
-        Serial.println(force[0]);
-        Serial.println(force[0]);
-    }default: {
+        Serial.print("Finger 1 Force: "); Serial.print(force[0]); Serial.print(" | " ); Serial.println(force[3]);
+        Serial.print("Finger 2 Force: "); Serial.print(force[1]); Serial.print(" | " ); Serial.println(force[4]);
+        Serial.print("Finger 3 Force: "); Serial.print(force[2]); Serial.print(" | " ); Serial.println(force[5]);
+
+        break;
+    }    default: {
         //do something
         Serial.println("ERROR, User input was incorrect");
         directions[0] = false;
@@ -122,33 +118,41 @@ void userMotorChoice(char userInput, int (*motors), boolean directions[], int (*
 }
 
 void checkPressure(int (*force)){
-  force[0] = analogRead(A2);//1-6
-  force[1] = analogRead(A0);//2-4
+  //Motors 1 and 6
+  force[0] = analogRead(A0);//1-6
+  force[3] = analogRead(A3);
+
+  //Motors 2 and 4
+  force[1] = analogRead(A2);//2-4
+  force[4] = analogRead(A5);
+
+  //Motors 3 and 5
   force[2] = analogRead(A1);//3-5
+  force[5] = analogRead(A4);
+
 }
 
 
 
 void displayMenu(){
-  Serial.println("Enter number for control option:");
-  Serial.println("0. Print Positions");
-  Serial.println("1. Motor 1, Clockwise");
-  Serial.println("2. Motor 2, Clockwise");
-  Serial.println("3. Motor 3, Clockwise");
-  Serial.println("4. Motor 4, Clockwise");
-  Serial.println("5. Motor 5, Clockwise");
-  Serial.println("6. Motor 6, Clockwise");
-  Serial.println("7. Motor 1, Counter-Clockwise");
-  Serial.println("8. Motor 2, Counter-Clockwise");
-  Serial.println("a. Motor 3, Counter-Clockwise");
-  Serial.println("b. Motor 4, Counter-Clockwise");
-  Serial.println("c. Motor 5, Counter-Clockwise");
-  Serial.println("d. Motor 6, Counter-Clockwise");
+  //Serial.println("Enter number for control option:");
+  //Serial.println("0. Print Positions");
+  //Serial.println("1. Motor 1, Clockwise");
+  //Serial.println("2. Motor 2, Clockwise");
+  //Serial.println("3. Motor 3, Clockwise");
+  //Serial.println("4. Motor 4, Clockwise");
+  //Serial.println("5. Motor 5, Clockwise");
+  //Serial.println("6. Motor 6, Clockwise");
+  //Serial.println("7. Motor 1, Counter-Clockwise");
+  //Serial.println("8. Motor 2, Counter-Clockwise");
+  //Serial.println("a. Motor 3, Counter-Clockwise");
+  //Serial.println("b. Motor 4, Counter-Clockwise");
+  //Serial.println("c. Motor 5, Counter-Clockwise");
+  //Serial.println("d. Motor 6, Counter-Clockwise");
   Serial.println("e. Curl Grab");
   Serial.println("z. Reset positions, claw limp");
   Serial.println("f. Close All Joints");
   Serial.println("h. Open All Joints");
-  Serial.println("i. Grab Object");
   Serial.println("k. Show Pressure Values");
   Serial.println();
 }
